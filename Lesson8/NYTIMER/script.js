26cb3d475d300e196039002757200aab4e9884e2
TehBez <ruslan.ponomar@gmail.com>
2020-06-01 14:58:34 +0300

    Lesson8 - HW - fixed

--- Lesson8/NYTIMER/script.js
+++ Lesson8/NYTIMER/script.js
@@ -1,6 +1,8 @@
+// Задаём конечную дату
 timeend = new Date();
 timeend = new Date(timeend.getYear()>1900?(timeend.getYear()+1):(timeend.getYear()+1901),0,1);
 
+// Функция таймера
 function time() {
 	today = new Date();
 	today = Math.floor((timeend-today)/1000);
@@ -8,7 +10,9 @@ function time() {
 	tmin = today % 60; today = Math.floor(today/60); if(tmin<10)tmin = '0' + tmin;
 	thour = today % 24; today = Math.floor(today/24);
 	timestr = today + " дней " + thour + " часов " + tmin + " минут " + tsec + " секунд";
-	document.getElementById('t').innerHTML = timestr;
-	window.setTimeout("time()", 1000);
+
+	document.getElementById('t').innerHTML = timestr; // почитать ещё про getElementById, innerHTML
+
+	window.setTimeout("time()", 1000); // почитать ещё про window.setTimeout
 }
 
